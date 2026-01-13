369ceb6616a25a5a334f70b6768558202a2a36cb
voidnamespace <voidnamespace@outlook.com>
2026-01-13 17:20:01 +0400

    123

--- src/AccountService.Domain/ValueObjects/AccountNumberVO.cs
+++ src/AccountService.Domain/ValueObjects/AccountNumberVO.cs
@@ -2,4 +2,20 @@
 
 public class AccountNumberVO
 {
+
+    public string _accountNumber { get; private set; }
+    private AccountNumberVO() { }
+
+    public string Value => _accountNumber;
+
+    public AccountNumberVO(string accountNumber)
+    {
+        if (string.IsNullOrWhiteSpace(accountNumber))
+            throw new ArgumentNullException("account number is required");
+        if (accountNumber.Length != 12)
+            throw new ArgumentException("symbol's limit reached");
+        if (accountNumber.All(char.IsDigit))
+            throw new ArgumentException("Account number must contain only digits");
+    }
+
 }
