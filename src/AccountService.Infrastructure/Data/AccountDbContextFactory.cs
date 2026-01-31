741fe4f1b63369dfbd41d5013fcbcf126edd80dd
voidnamespace <voidnamespace@outlook.com>
2026-01-31 15:00:10 +0400

    111

---
+++ src/AccountService.Infrastructure/Data/AccountDbContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+
+namespace AccountService.Infrastructure.Data;
+
+public class AccountDbContextFactory
+    : IDesignTimeDbContextFactory<AccountDbContext>
+{
+    public AccountDbContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true)
+            .AddJsonFile("appsettings.Development.json", optional: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        var connectionString =
+            configuration.GetConnectionString("accountdb");
+
+        var options = new DbContextOptionsBuilder<AccountDbContext>()
+            .UseNpgsql(connectionString)
+            .Options;
+
+        return new AccountDbContext(options);
+    }
+}
