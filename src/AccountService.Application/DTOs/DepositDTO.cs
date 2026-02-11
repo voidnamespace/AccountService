cf1ba394ebd79f2eb2daf997aa45cd099b5981a8
voidnamespace <voidnamespace@outlook.com>
2026-02-11 12:46:45 +0400

    heeeaaa

--- src/AccountService.Application/DTOs/DepositDTO.cs
+++ src/AccountService.Application/DTOs/DepositDTO.cs
@@ -1,8 +1,10 @@
-﻿namespace AccountService.Application.DTOs;
+﻿using AccountService.Domain.Enums;
+
+namespace AccountService.Application.DTOs;
 
 public class DepositRequest
 {
     public string AccountNumber { get; init; } = string.Empty;
     public decimal Amount { get; init; }
-    public string Currency { get; init; } = string.Empty;
+    public Currency Currency { get; init; } 
 }
